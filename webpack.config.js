1ab0398645ff157b0db47be7143d80270f86bc1d
5nYqnHvk <titivoot@fastmail.com>
2024-03-24 21:59:25 +0700

    add dist

--- webpack.config.js
+++ webpack.config.js
@@ -1,52 +1,52 @@
-const webpack = require('webpack');
+const webpack = require("webpack");
 const TerserPlugin = require("terser-webpack-plugin");
 
 const path = require("path");
-const metadata = require('./config/metadata.cjs')
+const metadata = require("./config/metadata.cjs");
 
 const config = {
-    entry     : "./src/index.ts",
-    output    : {
-        path     : path.resolve(__dirname, "dist"),
-        filename : "bundle.js"
-    },
-    devServer : {
-        open : true,
-        host : "localhost",
-    },
-    plugins   : [
-        new webpack.ProvidePlugin({
-            $      : "jquery",
-            jQuery : "jquery",
-            toastr : "toastr",
-            swal   : "sweetalert",
-        })
+  entry: "./src/index.ts",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "index.user.js",
+  },
+  devServer: {
+    open: true,
+    host: "localhost",
+  },
+  plugins: [
+    new webpack.ProvidePlugin({
+      $: "jquery",
+      jQuery: "jquery",
+      toastr: "toastr",
+      swal: "sweetalert",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        exclude: /node_modules/,
+        loader: "babel-loader",
+      },
     ],
-    module    : {
-        rules : [
-            {
-                test   : /\.ts$/,
-                exclude: /node_modules/,
-                loader : "babel-loader",
-            },
-        ],
-    },
-    resolve   : {
-        extensions: ['.ts', '.js'],
-    },
-    optimization: {
-        minimize: true,
-        minimizer: [
-        new TerserPlugin({
-            terserOptions: {
-            format: {
-                comments: /@license/i,
-            },
-            },
-            extractComments: true,
-        }),
-        ],
-    },
+  },
+  resolve: {
+    extensions: [".ts", ".js"],
+  },
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new TerserPlugin({
+        terserOptions: {
+          format: {
+            comments: /@license/i,
+          },
+        },
+        extractComments: true,
+      }),
+    ],
+  },
 };
 
 module.exports = config;
