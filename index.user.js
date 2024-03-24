997e85c2a38b04f8ca1c207e4f3e24fae9b69867
5nYqnHvk <titivoot@fastmail.com>
2024-03-24 22:11:56 +0700

    add meta

---
+++ index.user.js
@@ -0,0 +1,20 @@
+// ==UserScript==
+// @name         Enhancer for TorrentDD
+// @version      1.0.0
+// @description  เพิ่มฟังก์ชั้นที่ทำให้ใช้เว็บได้ง่ายขึ้น
+// @author       5nYqnHvk
+// @match        https://www.torrentdd.com/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=torrentdd.com
+// @grant        GM_info
+// @grant        GM_addElement
+// @grant        GM_setValue
+// @grant        GM_getValue
+// @grant        GM_deleteValue
+// @grant        GM_registerMenuCommand
+// @grant        GM_unregisterMenuCommand
+// @grant        GM_notification
+// @require      https://raw.githubusercontent.com/Titivoot/Enhancer-for-TorrentDD/main/dist/index.user.js?version=1.0.0
+// @downloadURL  https://raw.githubusercontent.com/Titivoot/Enhancer-for-TorrentDD/main/index.user.js
+// @updateURL    https://raw.githubusercontent.com/Titivoot/Enhancer-for-TorrentDD/main/index.user.js
+// @connect      5nYqnHvk.xyz
+// ==/UserScript==
