c261fd3bd38c0a1ad3948f616e6e01f84bf201aa
5nYqnHvk <titivoot@fastmail.com>
2024-04-04 18:32:06 +0700

    1.3.6

--- index.user.js
+++ index.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Enhancer for TorrentDD
-// @version      1.3.5
+// @version      1.3.6
 // @description  เพิ่มฟังก์ชั้นที่ทำให้ใช้เว็บได้ง่ายขึ้น
 // @author       5nYqnHvk
 // @match        https://www.torrentdd.com/*
@@ -13,7 +13,7 @@
 // @grant        GM_registerMenuCommand
 // @grant        GM_unregisterMenuCommand
 // @grant        GM_notification
-// @require      https://raw.githubusercontent.com/Titivoot/Enhancer-for-TorrentDD/main/dist/index.user.js?version=1.3.5
+// @require      https://raw.githubusercontent.com/Titivoot/Enhancer-for-TorrentDD/main/dist/index.user.js?version=1.3.6
 // @downloadURL  https://raw.githubusercontent.com/Titivoot/Enhancer-for-TorrentDD/main/index.user.js
 // @updateURL    https://raw.githubusercontent.com/Titivoot/Enhancer-for-TorrentDD/main/index.user.js
 // @connect      5nYqnHvk.xyz
@@ -21,6 +21,11 @@
 
 /*
 Changelog
+1.3.6
+- แก้ไขปุ่มรับตั๋ว ไม่เปลี่ยนสี
+- แก้ไขข้อความแสดงไม่ถูกต้องเมื่อกดรับตั๋ว
+- เพิ่มสถิติการเดิมพัน ที่หน้า ebet
+
 1.3.5
 - แก้บัคกดรับตั๋ว 2 ครั้ง
 
